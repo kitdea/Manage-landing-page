605d5ff102fab0eb6fdd40592f5002a437cda08f
kitdea <francismarrosales@gmail.com>
2022-05-13 19:58:28 +0800

    js test animate

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const nav = document.getElementById('menu');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open');
-  nav.classList.toggle('flex').style('height: 100%');
+  nav.classList.toggle('height: 100%');
+  nav.classList.toggle('flex');
   nav.classList.toggle('hidden');
 });
 
