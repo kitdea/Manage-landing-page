22c3ce1f2745117dc98d74ab96f1bce21a193ef2
kitdea <francismarrosales@gmail.com>
2022-05-13 19:59:39 +0800

    undo everything

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const nav = document.getElementById('menu');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open');
-  nav.classList.toggle('height: 100%');
   nav.classList.toggle('flex');
   nav.classList.toggle('hidden');
 });
